896fdcdc7924b98fc5c843c1bb70af8b695b848a
prsantiago <prsantiago96@gmail.com>
2021-02-26 11:47:42 -0600

    generar clave y obtener comentarios

--- inc/funciones/funciones.php
+++ inc/funciones/funciones.php
@@ -41,6 +41,39 @@ function obtenerAlumnosRegistrados($universidad_usuario) {
     }
 }
 
+function obtenerAlumnoSeleccionado($id_alumno, $conn) {
+
+    try {
+        // return $conn->query("SELECT matricula, id_estado FROM alumno WHERE id = $id_alumno");
+        return array('id_estado' => 1, 'matricula' => '2163035605');
+    } catch(Exception $e) {
+        echo "Error!!!".$e->getMessage()."<br>";
+        return false;
+    }
+}
+function numeroDeProyectos($conn) {
+    try {
+        // return $conn->query("SELECT MAX(id) FROM proyecto_vigente");
+        return 1;
+    } catch(Exception $e) {
+        echo "Error!!!".$e->getMessage()."<br>";
+        return false;
+    }
+}
+
+function generarClaveProyecto($id_alumno, $universidad) {
+    require 'conexion.php';
+    $num_proyectos = numeroDeProyectos($conn);
+    $datos_alumno = obtenerAlumnoSeleccionado($id_alumno,$conn);
+
+    $charEstado = $datos_alumno['id_estado'] == 1 ? "P" : "T";
+    $finMatricula = substr($datos_alumno['matricula'], -4); // ultimos 4 caracteres de la matricula
+
+    $clave = $charEstado.$universidad.'-'.$finMatricula.$num_proyectos;
+    // $clave = strval($id_alumno).$universidad;
+    return $clave;
+}
+
 function obtenerFechaSeguimiento($id_proyecto, $id_entrega, $id_proceso){
     require 'conexion.php';
     try {
@@ -61,10 +94,24 @@ function obtenerSeguimientos($id_proyecto){
     }
 }
 
-function obtenerComentarios($id_seguimiento) {
+function obtenerComentariosEtapa($id_proyecto, $id_etapa) {
     include 'conexion.php';
     try {
-        return $conn->query("SELECT id, nombre, apellido, comentario, fecha FROM comentario_vigente WHERE id_seguimiento = $id_seguimiento");
+        return $conn->query("SELECT id, nombre, apellido, comentario, fecha 
+                            FROM (comentario_vigente INNER JOIN seguimiento_vigente ON comentario_vigente.id_proyecto = seguimiento_vigente.id_proyecto) 
+                            WHERE comentario_vigente.id_proyecto = $id_proyecto AND seguimiento_vigente.id_entrega = $id_etapa");
+    } catch(Exception $e) {
+        echo "Error!!!".$e->getMessage()."<br>";
+        return false;
+    }
+}
+
+function obtenerComentariosActividad($id_proyecto, $id_etapa, $id_actividad) {
+    include 'conexion.php';
+    try {
+        return $conn->query("SELECT id, nombre, apellido, comentario, fecha 
+                            FROM (comentario_vigente INNER JOIN seguimiento_vigente ON comentario_vigente.id_proyecto = seguimiento_vigente.id_proyecto) 
+                            WHERE comentario_vigente.id_proyecto = $id_proyecto AND seguimiento_vigente.id_entrega = $id_etapa AND seguimiento_vigente.id_proceso = $id_actividad");
     } catch(Exception $e) {
         echo "Error!!!".$e->getMessage()."<br>";
         return false;
