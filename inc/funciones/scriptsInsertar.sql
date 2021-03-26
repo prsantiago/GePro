c28d27546846ee9a0eb0f41cd54149f084bc935e
prsantiago <prsantiago96@gmail.com>
2021-03-26 16:08:52 -0600

    checando gitignore

--- inc/funciones/scriptsInsertar.sql
+++ inc/funciones/scriptsInsertar.sql
@@ -1,25 +1,8 @@
-CREATE TABLE comentario_vigente(
-	id INT NOT NULL AUTO_INCREMENT,
-    id_proyecto INT,
-    nombre VARCHAR(75),
-    apellido VARCHAR(75),
-    comentario VARCHAR(255),
-    fecha DATETIME,
-
-	PRIMARY KEY(id)
-);
-
-CREATE TABLE comentario_historico(
-	id INT NOT NULL AUTO_INCREMENT,
-    id_proyecto INT,
-    nombre VARCHAR(75),
-    apellido VARCHAR(75),
-    comentario VARCHAR(255),
-    fecha DATETIME,
-
-	PRIMARY KEY(id)
-);
-
--- Llaves foráneas comentarios
-ALTER TABLE comentario_vigente ADD CONSTRAINT FK_comentario FOREIGN KEY (id_proyecto) references proyecto_vigente(id);
-ALTER TABLE comentario_historico ADD CONSTRAINT FK_comentario_historico FOREIGN KEY (id_proyecto) references proyecto_historico(id);
+DELIMITER //
+    CREATE PROCEDURE BORRAR_PROYECTO(id_proyecto int)
+    BEGIN
+        DELETE FROM comentario_vigente WHERE id_proyecto = id_proyecto;
+        DELETE FROM seguimiento_vigente WHERE id_proyecto = id_proyecto;
+        DELETE FROM proyecto_vigente WHERE id = id_proyecto;
+    END//
+ DELIMITER ;
