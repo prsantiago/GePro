d082fccc29b546c8e46a6f95b9433508f7ef990e
prsantiago <prsantiago96@gmail.com>
2021-09-06 19:05:13 -0500

    nuevos procedures

--- inc/funciones/bd-gepro.sql
+++ inc/funciones/bd-gepro.sql
@@ -1,123 +1,173 @@
--- CREATE DATABASE deni;
+CREATE DATABASE deni;
+USE deni;
 
--- USE deni;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Table structure for table `profesor`
+--
 
 CREATE TABLE profesor(
-	id INT NOT NULL auto_increment,
-	matricula INT,
-    nombre VARCHAR(75),
-    apellido VARCHAR(125),
-    correo VARCHAR(125),
-    contraseña VARCHAR(75),
-    universidad VARCHAR(125),
-    division VARCHAR(125),
-    departamento VARCHAR(125),
+    id INT NOT NULL auto_increment,
+    matricula VARCHAR(25) DEFAULT NULL,
+    nombre VARCHAR(50) DEFAULT NULL,
+    apellido VARCHAR(50) DEFAULT NULL,
+    correo VARCHAR(50) DEFAULT NULL,
+    contraseña VARCHAR(255) DEFAULT NULL,
+    universidad VARCHAR(10) DEFAULT NULL,
+    division VARCHAR(50) DEFAULT NULL,
+    departamento VARCHAR(50) DEFAULT NULL,
     
     PRIMARY KEY(id)
-);
+)DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `alumno`
+--
 
 CREATE TABLE alumno(
-	id INT NOT NULL AUTO_INCREMENT,
-    matricula INT,
-    nombre VARCHAR(75),
-    apellido VARCHAR(125),
-	correo VARCHAR(125),
-    contraseña VARCHAR(255),
-    universidad VARCHAR(125),
-    division VARCHAR(125),
-    carrera VARCHAR(125),
-    id_estado INT,
+    id INT NOT NULL AUTO_INCREMENT,
+    matricula VARCHAR(25) DEFAULT NULL,
+    nombre VARCHAR(50) DEFAULT NULL,
+    apellido VARCHAR(50) DEFAULT NULL,
+    correo VARCHAR(50) DEFAULT NULL,
+    contraseña VARCHAR(255) DEFAULT NULL,
+    universidad VARCHAR(10) DEFAULT NULL,
+    division VARCHAR(50) DEFAULT NULL,
+    carrera VARCHAR(50) DEFAULT NULL,
+    id_estado INT DEFAULT NULL,
     
     PRIMARY KEY(id)
-);
+)DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `proyecto_vigente`
+--
 
 CREATE TABLE proyecto_vigente(
-	id INT NOT NULL AUTO_INCREMENT,
-    id_asesor1 INT,
-    id_asesor2 INT,
-    id_alumno INT,
-    nombre VARCHAR(125),
-    fechaInicio DATE,
-    fechaFin DATE,
-    descripcion VARCHAR(255),
-    comentarioFinal VARCHAR(255),
-    clave VARCHAR(10),
+    id INT NOT NULL AUTO_INCREMENT,
+    id_asesor1 INT DEFAULT NULL,
+    id_asesor2 INT DEFAULT NULL,
+    id_alumno INT DEFAULT NULL,
+    proyecto VARCHAR(125) DEFAULT NULL,
+    fechaInicio DATE DEFAULT NULL,
+    fechaFin DATE DEFAULT NULL,
+    descripcion VARCHAR(255) DEFAULT NULL,
+    comentarioFinal VARCHAR(255) DEFAULT NULL,
+    clave VARCHAR(15) DEFAULT NULL,
     
     PRIMARY KEY(id)
-);
+)DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `proyecto_historico`
+--
 
 CREATE TABLE proyecto_historico(
-	id INT NOT NULL AUTO_INCREMENT,
-    id_asesor1 INT,
-    id_asesor2 INT,
-    id_alumno INT,
-    nombre VARCHAR(125),
-    fechaInicio DATE,
-    fechaFin DATE,
-    descripcion VARCHAR(255),
-    comentarioFinal VARCHAR(255),
-    clave VARCHAR(10),
+    id INT NOT NULL AUTO_INCREMENT,
+    id_asesor1 INT DEFAULT NULL,
+    id_asesor2 INT DEFAULT NULL,
+    id_alumno INT DEFAULT NULL,
+    proyecto VARCHAR(125) DEFAULT NULL,
+    fechaInicio DATE DEFAULT NULL,
+    fechaFin DATE DEFAULT NULL,
+    descripcion VARCHAR(255) DEFAULT NULL,
+    comentarioFinal VARCHAR(255) DEFAULT NULL,
+    clave VARCHAR(15) DEFAULT NULL,
     
     PRIMARY KEY(id)
-);
+)DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `seguimiento_vigente`
+--
 
 CREATE TABLE seguimiento_vigente(
     id INT NOT NULL AUTO_INCREMENT,
-    id_proyecto INT,
-    fecha_entrega DATE,
-    proxima_entrega DATE,
-    id_etapa INT,
-    id_actividad INT,
+    id_proyecto INT DEFAULT NULL,
+    fecha_entrega DATE DEFAULT NULL,
+    proxima_entrega DATE DEFAULT NULL,
+    id_etapa INT DEFAULT NULL,
+    id_actividad INT DEFAULT NULL,
 
     PRIMARY KEY(id)
-);
+)DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `seguimiento_historico`
+--
 
 CREATE TABLE seguimiento_historico(
     id INT NOT NULL AUTO_INCREMENT,
-    id_proyecto INT,
-    fecha_entrega DATE,
-    proxima_entrega DATE,
-    id_etapa INT,
-    id_actividad INT,
+    id_proyecto INT DEFAULT NULL,
+    fecha_entrega DATE DEFAULT NULL,
+    proxima_entrega DATE DEFAULT NULL,
+    id_etapa INT DEFAULT NULL,
+    id_actividad INT DEFAULT NULL,
 
     PRIMARY KEY(id)
-);
+)DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `etapa`
+--
 
 CREATE TABLE etapa(
-	id INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(75),
-    
-	PRIMARY KEY(id)
-);
-
-CREATE TABLE actividad(
-	id INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(75),
-    tiempo_maximo VARCHAR(50),
-    responsable INT,
-    
-	PRIMARY KEY(id)
-);
-
-CREATE TABLE estado(
-	id INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(25),
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(20) DEFAULT NULL,
     
     PRIMARY KEY(id)
-);
+)DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `actividad`
+--
+
+CREATE TABLE actividad(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre varchar(20) DEFAULT NULL,
+    tiempo_maximo varchar(20) DEFAULT NULL,
+    responsable INT  DEFAULT NULL,
+    
+    PRIMARY KEY(id)
+)DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `estado`
+--
+
+CREATE TABLE estado(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(25) DEFAULT NULL,
+    
+    PRIMARY KEY(id)
+)DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `comentario`
+--
 
 CREATE TABLE comentario(
     id INT NOT NULL AUTO_INCREMENT,
-    id_proyecto INT,
-    id_etapa INT,
-    id_actividad INT,
-    nombre VARCHAR(75),
-    apellido VARCHAR(125),
-    comentario VARCHAR(255),
-    fecha datetime,
+    id_proyecto INT DEFAULT NULL,
+    id_etapa INT DEFAULT NULL,
+    id_actividad INT DEFAULT NULL,
+    nombre VARCHAR(50) DEFAULT NULL,
+    apellido VARCHAR(50) DEFAULT NULL,
+    comentario VARCHAR(255) DEFAULT NULL,
+    fecha datetime DEFAULT NULL,
 
     PRIMARY KEY(id)
-);
+)DEFAULT CHARSET=utf8;
+
+CREATE TABLE clave_password(
+    id INT NOT NULL AUTO_INCREMENT,
+    correo VARCHAR(50),
+    clave VARCHAR(50),
+    
+    PRIMARY KEY(id)
+)DEFAULT CHARSET=utf8;
 
 -- Llaves foráneas alumno
 ALTER TABLE alumno ADD CONSTRAINT FK_estado FOREIGN KEY (id_estado) references estado(id);
@@ -142,26 +192,98 @@ ALTER TABLE comentario ADD CONSTRAINT FK_comen_proy FOREIGN KEY (id_proyecto) re
 ALTER TABLE comentario ADD CONSTRAINT FK_comen_et FOREIGN KEY (id_etapa) references etapa(id);
 ALTER TABLE comentario ADD CONSTRAINT FK_comen_act FOREIGN KEY (id_actividad) references actividad(id);
 
--- valores a tablas catálogo
-INSERT INTO estado VALUES
-(NULL, 'Pre-tesista'),(NULL,'Tesista'),(NULL,'Titulado');
+--
+-- Dumping data for table `actividad`
+--
 
-INSERT INTO etapa VALUES
-(NULL,'Introducción'),
-(NULL,'Marco teórico'),
-(NULL,'Desarrollo'),
-(NULL,'Resultados'),
-(NULL,'Tesis integrada');
+INSERT INTO actividad (id, nombre, tiempo_maximo, responsable) VALUES
+(1, 'Entrega', '2 semanas', 1),
+(2, 'Retroalimentación', '1 semana', 2),
+(3, 'Aprobación', '1 semana', 2),
+(4, 'Presentación', '2 semanas', 1);
 
-INSERT INTO actividad VALUES
-(NULL,'Entrega','2 semanas',1),
-(NULL,'Retroalimentación','1 semana',2),
-(NULL,'Aprobación','1 semana',2),
-(NULL,'Presentación','2 semanas',1);
+--
+-- Dumping data for table `estado`
+--
 
-DELIMITER //
-    CREATE PROCEDURE NUEVO_SEGUIMIENTO(idSeg int,idProy int, id_etapa int,id_actividad int,fecha date,com varchar(255),aprobado bool)
-    BEGIN
+INSERT INTO estado (id, nombre) VALUES
+(1, 'Pre-tesista'),
+(2, 'Tesista'),
+(3, 'Titulado');
+
+--
+-- Dumping data for table `etapa`
+--
+
+INSERT INTO etapa (id, nombre) VALUES
+(1, 'Introducción'),
+(2, 'Marco teórico'),
+(3, 'Desarrollo'),
+(4, 'Resultados'),
+(5, 'Tesis integrada');
+
+-- --------------------------------------------------------
+
+DELIMITER $$
+
+--
+-- Procedures
+--
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `BORRAR_PROYECTO` (`idProyecto` INT)  BEGIN
+        DELETE FROM comentario WHERE id_proyecto = idProyecto;
+        DELETE FROM seguimiento_vigente WHERE id_proyecto = idProyecto;
+        DELETE FROM proyecto_vigente WHERE id = idProyecto;
+    END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `FINALIZAR_PROYECTO` (`idProy` INT)  BEGIN
+        DECLARE X INT;
+        DECLARE Y INT;
+        SELECT id_alumno INTO X FROM proyecto_vigente WHERE id = idProy;
+        SELECT id_estado INTO Y FROM alumno WHERE id = X; 
+        IF (Y = 1) THEN
+            UPDATE alumno SET id_estado = 2 WHERE id = X;
+        ELSE 
+            UPDATE alumno SET id_estado = 3 WHERE id = X;
+        END IF;
+        INSERT INTO proyecto_historico SELECT * FROM proyecto_vigente WHERE id = idProy;
+        INSERT INTO seguimiento_historico SELECT * FROM seguimiento_vigente WHERE id_proyecto = idProy;
+        DELETE FROM seguimiento_vigente WHERE id_proyecto = idProy; 
+        DELETE FROM comentario WHERE id_proyecto = idProy;
+        DELETE FROM proyecto_vigente WHERE id = idProy;
+    END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `NUEVA_PASSWORD` (`pwd` VARCHAR(255), `_correo` VARCHAR(50))  BEGIN
+        DECLARE X INT;
+        DECLARE Y INT;
+        SELECT id INTO X FROM profesor WHERE correo = _correo;
+        SELECT IFNULL(X,1) INTO Y;
+        IF (Y = 1) THEN
+            SELECT id INTO X FROM alumno WHERE correo = _correo;
+            UPDATE alumno SET contraseña = pwd WHERE id = X;
+            DELETE FROM clave_password WHERE correo = _correo;
+        ELSE
+            UPDATE profesor SET contraseña = pwd WHERE id = X;
+            DELETE FROM clave_password WHERE correo = _correo;
+        END IF;
+    END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `NUEVO_COMENTARIO` (IN `nombre_usuario` VARCHAR(50), IN `apellido_usuario` VARCHAR(50), IN `id_proy` INT, IN `id_etapa` INT, IN `id_actividad` INT, IN `comentario` VARCHAR(255))  BEGIN
+        INSERT INTO comentario
+        VALUES (NULL, id_proy, id_etapa, id_actividad, nombre_usuario, apellido_usuario, comentario, NOW());
+    END$$ 
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `NUEVO_PROYECTO` (IN `id_asesor1` INT, IN `id_asesor2` INT, IN `id_alumno` INT, IN `nombre_proyecto` VARCHAR(125), IN `fecha` VARCHAR(100), IN `descripcion` VARCHAR(255), IN `universidad_alumno` VARCHAR(10), IN `clave_proy` VARCHAR(15))  BEGIN
+        INSERT INTO proyecto_vigente (id_asesor1, id_asesor2, id_alumno, proyecto, fechaInicio, descripcion, clave) 
+        VALUES (id_asesor1, id_asesor2, id_alumno, nombre_proyecto, fecha, descripcion, clave_proy);
+        
+        SELECT @max_proyecto:=MAX(id) from proyecto_vigente;
+        SELECT @fechaInicio:=fechaInicio FROM proyecto_vigente WHERE id = @max_proyecto;
+        SELECT @fechaEntrega:=DATE_ADD(@fechaInicio, INTERVAL 14 DAY);  
+        INSERT INTO seguimiento_vigente VALUES (NULL,@max_proyecto,NULL,@fechaEntrega,1,1);
+    END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `NUEVO_SEGUIMIENTO` (`idSeg` INT, `idProy` INT, `id_etapa` INT, `id_actividad` INT, `fecha` DATE, `com` VARCHAR(255), `aprobado` BOOL)  BEGIN
         DECLARE X DATE;
         DECLARE Y INT;
         IF (aprobado = false) THEN
@@ -182,59 +304,17 @@ DELIMITER //
                 IF (id_actividad = 4) THEN
                     UPDATE seguimiento_vigente SET fecha_entrega = fecha WHERE id=idSeg; 
                     UPDATE proyecto_vigente SET fechaFin = fecha,comentarioFinal = com WHERE id=idProy; 
-                    -- CALL HISTORICOS(idProy);
                 ELSE
                     INSERT INTO seguimiento_vigente VALUES (NULL,idProy,NULL,X,id_etapa,4);
                     UPDATE seguimiento_vigente SET fecha_entrega = fecha,id_actividad = 3 WHERE id=idSeg; 
                 END IF;
             END IF;
         END IF;
-    END//
- DELIMITER ; 
+    END$$
 
- DELIMITER //
-    CREATE PROCEDURE FINALIZAR_PROYECTO(idProy int)
-    BEGIN
-        DECLARE X INT;
-        DECLARE Y INT;
-        SELECT id_alumno INTO X FROM proyecto_vigente WHERE id = idProy;
-        SELECT id_estado INTO Y FROM alumno WHERE id = X; 
-        IF (Y = 1) THEN
-            UPDATE alumno SET id_estado = 2 WHERE id = X;
-        ELSE 
-            UPDATE alumno SET id_estado = 3 WHERE id = X;
-        END IF;
-        INSERT INTO proyecto_historico SELECT * FROM proyecto_vigente WHERE id = idProy;
-        INSERT INTO seguimiento_historico SELECT * FROM seguimiento_vigente WHERE id_proyecto = idProy;
-        DELETE FROM seguimiento_vigente WHERE id_proyecto = idProy; 
-        DELETE FROM comentario WHERE id_proyecto = idProy;
-        DELETE FROM proyecto_vigente WHERE id = idProy;
-    END//
-DELIMITER ;
-
-DELIMITER //
-    CREATE PROCEDURE NUEVO_COMENTARIO(nombre_usuario varchar(75),apellido_usuario varchar(75), 
-                                        id_proy int, id_etapa int, id_actividad int, comentario varchar(255))
-    BEGIN
-        INSERT INTO comentario
-        VALUES (NULL, id_proy, id_etapa, id_actividad, nombre_usuario, apellido_usuario, comentario, NOW());
-    END//
-DELIMITER ; 
-
-DELIMITER //
-    CREATE PROCEDURE BORRAR_PROYECTO(idProyecto int)
-    BEGIN
-        DELETE FROM comentario WHERE id_proyecto = idProyecto;
-        DELETE FROM seguimiento_vigente WHERE id_proyecto = idProyecto;
-        DELETE FROM proyecto_vigente WHERE id = idProyecto;
-    END//
- DELIMITER ;
-
-DELIMITER //
-	CREATE PROCEDURE OBTENER_DETALLES_PROYECTO(id_asesor int)
-	BEGIN
-		SELECT 
-		proyecto_vigente.id,
+CREATE DEFINER=`root`@`localhost` PROCEDURE `OBTENER_DETALLES_PROYECTO` (`id_asesor` INT)  BEGIN
+        SELECT 
+        proyecto_vigente.id,
         proyecto_vigente.clave,
         proyecto_vigente.proyecto,
         alumno.nombre,
@@ -244,35 +324,17 @@ DELIMITER //
         alumno ON proyecto_vigente.id_alumno=alumno.id)
     WHERE
         profesor.id = id_asesor;
-	END//
-DELIMITER ;
+    END$$
 
-DELIMITER //
-    CREATE PROCEDURE NUEVO_PROYECTO(id_asesor1 int, id_asesor2 int, id_alumno int, nombre_proyecto varchar(125),
-                                    fecha varchar(100), descripcion varchar(255), universidad_alumno varchar(10), clave_proy varchar(25))
-    BEGIN
-        INSERT INTO proyecto_vigente (id_asesor1, id_asesor2, id_alumno, proyecto, fechaInicio, descripcion, clave) 
-        VALUES (id_asesor1, id_asesor2, id_alumno, nombre_proyecto, fecha, descripcion, clave_proy);
-        
-        SELECT @max_proyecto:=MAX(id) from proyecto_vigente;
-        SELECT @fechaInicio:=fechaInicio FROM proyecto_vigente WHERE id = @max_proyecto;
-        SELECT @fechaEntrega:=DATE_ADD(@fechaInicio, INTERVAL 14 DAY);  
-        INSERT INTO seguimiento_vigente VALUES (NULL,@max_proyecto,NULL,@fechaEntrega,1,1);
-    END//
- DELIMITER ;   
-
-DELIMITER //
-    CREATE PROCEDURE NUEVA_PASSWORD(pwd varchar(255),_correo varchar(50))
-    BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GUARDAR_CLAVE` (`_correo` VARCHAR(50),`_clave` VARCHAR(50))  
+BEGIN
         DECLARE X INT;
-        DECLARE Y INT;
-        SELECT id INTO X FROM profesor WHERE correo = _correo;
-        SELECT IFNULL(X,2) INTO Y;
-        IF (Y = 2) THEN
-            SELECT id INTO X FROM alumno WHERE correo = _correo;
-            UPDATE alumno SET contraseña = pwd WHERE id = X;
+        SELECT IFNULL(id,1) INTO X FROM clave_password WHERE correo = _correo;
+        IF (X != 1) THEN
+            UPDATE clave_password SET clave = _clave WHERE correo = _correo;
         ELSE
-            UPDATE profesor SET contraseña = pwd WHERE id = X;
+            INSERT INTO clave_password VALUES (null, _correo, _clave);
         END IF;
-    END//
- DELIMITER ;
+    END$$
+
+DELIMITER ;
