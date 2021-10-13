4a4a5b88824caef7225cbd3c8f1680b2e95d6932
prsantiago <prsantiago96@gmail.com>
2021-10-13 05:52:39 -0500

    cambio menor

--- recuperar-cuenta.php
+++ recuperar-cuenta.php
@@ -15,7 +15,7 @@ $_SESSION['user'] = $_GET['user'];
 <main class="bg-secundario contenedor-main">
     <div class="bg-terciario contenedor contenido sombra">
         <!-- <p><?php print_r($_SESSION); ?></p> -->
-        <br><p>Ingrese el correo institucional con el que se registró</p>
+        <br><p>Ingrese el correo institucional con el que se registró.</p>
         <form id="recuperar-cuenta" class="caja-recuperar" method="post">
             <div class="campo">
                 <label for="usuario">Correo institucional: </label>
